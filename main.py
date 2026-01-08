bf1d863d9a862f921fa87f992e2f428ef4793ed2
Mirko Manalo <mirko.manalo@gmail.com>
2026-01-08 10:28:38 +0100

    Add survival mode loop and loot system

--- main.py
+++ main.py
@@ -37,7 +37,11 @@ class Hero(Character):
             print(f"Hero used a potion. HP: {self._current_hp}")
         else:
             print(f"No potions left")
-                
+    
+    def add_potion(self) -> None:
+        self._potions += 1
+        print(f"{self._name} found a potion!")
+
 class Monster(Character):
     def __init__(self, name: str, max_hp: int, strength: int) -> None:
         super().__init__(name, max_hp, strength)
@@ -49,35 +53,52 @@ class Monster(Character):
             target.receive_damage(double_damage)
         else:
             super().attack(target)
+
+    def drop_loot(self) -> bool:
+        if random.random() < 0.3:
+            return True
+        return False
+
         
 if __name__ == "__main__":
     hero_name = input("What is the Hero name? ")
-    hero = Hero(hero_name, 100, 15, 2)
-    monster = Monster("Goblin", 60, 10)
+    hero = Hero(hero_name, 150, 15, 4)
+    kill_count = 0
 
-    print(f"Battle start")
+    monsters_name = ["Goblin", "Orc", "Skeleton"]
 
-    while hero.is_alive() and monster.is_alive():
-        print(hero)
-        print(monster)
+    while hero.is_alive():
+        monster = Monster(random.choice(monsters_name), random.randint(40, 100), random.randint(3, 15))
 
-        action = input(f"Press A to attack or H to heal: ")
-        if action.upper() == "A":
-            hero.attack(monster)
-        elif action.upper() == "H":
-            hero.heal()
-        else:
-            print("Action isn't valid. Try A or H")
-            continue
+        print(f"A wild {monster._name} appears!")
+
+        while hero.is_alive() and monster.is_alive():
+            print(hero)
+            print(monster)
+
+            action = input(f"Press A to attack or H to heal: ")
+            if action.upper() == "A":
+                hero.attack(monster)
+            elif action.upper() == "H":
+                hero.heal()
+            else:
+                print("Action isn't valid. Try A or H")
+                continue
+            
+            if monster.is_alive():
+                monster.attack(hero)
+                print("-" * 20)
         
-        if monster.is_alive():
-            monster.attack(hero)
-            print("-" * 20)
-    
-    if hero.is_alive():
-        print(f"Hero wins")
-    else:
-        print(f"Hero died")
+        if hero.is_alive():
+            print(f"You killed the monster!")
+            kill_count += 1
+            if monster.drop_loot():
+                hero.add_potion()
+            input(f"Press enter to continue...")
+        else:
+            print(f"Hero died")
+
+    print(f"\nGAME OVER. Total kills: {kill_count}")
 
 
         
